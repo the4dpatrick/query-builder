607f73bdf288599559cd705b4532e28b028497a6
Patrick Perey <the4dpatrick@yahoo.com>
2016-11-20 15:59:22 -0800

    Update Server: CORS, console.log query params

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 8080
 
+app.use(cors())
+
 app.get('/', function (req, res) {
   console.log('Query:', JSON.stringify(req.query, null, 2))
+  res.send('Success')
 })
 
 app.listen(PORT, function () {
