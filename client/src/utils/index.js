089b6deaa2152685238eab937d30a95beaed8b09
Patrick Perey <the4dpatrick@yahoo.com>
2016-11-22 11:13:47 -0800

    update https api url

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import qs from 'qs'
 const isDev = process.env.NODE_ENV === 'development'
-const API_URL = isDev ? 'http://localhost:8080/' : 'http://querybuilder-93475.onmodulus.net/'
+const API_URL = isDev ? 'http://localhost:8080/' : 'https://querybuilder-93475.onmodulus.net/'
 
 export function search(query) {
   return axios.get(`${API_URL}?${qs.stringify(query)}`)
